b871d32f4c39ea68da18f1ae22dbe711f3b5c667
Kaio Delphino <github@kaiodelphino.com>
2024-01-27 16:50:50 -0300

    initial commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        const vec = new Vector2(10, 20);
+        Debug.Log(vec.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
